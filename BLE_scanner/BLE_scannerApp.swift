655feaf1fe9d90984d3d8b2cc6e95610b64d3efc
Stephane BARTHELEMY <stephane@sbarthelemy.com>
2023-07-27 18:55:33 +0200

    Initial Commit

---
+++ BLE_scanner/BLE_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLE_scannerApp.swift
+//  BLE_scanner
+//
+//  Created by Stephane BARTHELEMY on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BLE_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
